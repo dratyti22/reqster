894e6634fdfb0da65f95bd0cfd5435c81e302c22
dratyti22 <belousovi783@gmail.com>
2024-11-13 19:51:20 +0300

    feat: structure for data parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use anyhow::Result;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"))]
+#[command(version = env!("CARGO_PKG_VERSION"))]
+struct Cli {
+    #[arg(short, long, help = "url to which the request is sent")]
+    url: String,
+}
 
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    let cli: Cli = Cli::parse();
+
+    println!("{:#?}", cli);
+    Ok(())
 }
