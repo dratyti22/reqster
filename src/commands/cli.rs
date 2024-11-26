27a25a000472aac417a5aed537e5cc77dec6ea16
dratyti22 <belousovi783@gmail.com>
2024-11-26 19:25:15 +0300

    feat: http method

--- src/commands/cli.rs
+++ src/commands/cli.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Parser, Debug)]
 #[command(name = env!("CARGO_PKG_NAME"), about = env!("CARGO_PKG_DESCRIPTION"))]
@@ -6,10 +6,30 @@ use clap::Parser;
 pub struct Cli {
     #[arg(short, long, help = "url to which the request is sent")]
     url: String,
+
+    #[arg(
+        short,
+        long,
+        help = "method to use for the request (GET, POST, PUT, DELETE)",
+        required = false,
+        default_value = "get"
+    )]
+    method: HttpMethod,
 }
 
 impl Cli {
     pub fn get_url(&self) -> &str {
         &self.url
     }
+    pub fn get_method(&self) -> &HttpMethod {
+        &self.method
+    }
+}
+
+#[derive(Parser, Debug, ValueEnum, Clone)]
+pub enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
 }
--- src/run.rs
+++ src/run.rs
@@ -2,9 +2,10 @@ use crate::commands::cli::Cli;
 use anyhow::Result;
 
 pub fn run(cli: Cli) -> Result<()> {
-
     let url = cli.get_url();
+    let method = cli.get_method();
     println!("URL {}", url);
+    println!("URL {:?}", method);
 
     Ok(())
 }
